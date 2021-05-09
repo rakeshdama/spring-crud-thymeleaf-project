0ddf1937abd8c2a19579621b511e8ba7d9b9e7b0
Aryan Akshay <varyanakshay@gmail.com>
2021-05-09 21:09:32 +0530

    CONTROLLER -> @GetMapping UPDATE EMPLOYEE

--- src/main/java/com/example/springboot/controller/EmployeeController.java
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.springboot.model.Employee;
@@ -35,4 +36,11 @@ public class EmployeeController {
 		employeeService.saveEmployee(employee);
 		return "redirect:/";
 	}
+	 @GetMapping("/showFormForUpdate/{id}")
+	    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+		 Employee employee = employeeService.getEmployeeById(id);
+	        model.addAttribute("employee", employee);
+	        return "update_employee";
+	    }
+
 }
