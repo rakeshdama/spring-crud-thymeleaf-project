df480fa2c56147c6025f21c1227c91e9ce870a7b
Aryan Akshay <varyanakshay@gmail.com>
2021-05-09 20:48:06 +0530

    @PostMapping("/saveEmployee")

--- src/main/java/com/example/springboot/controller/EmployeeController.java
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.springboot.model.Employee;
 import com.example.springboot.service.EmployeeService;
 
 @Controller
@@ -15,9 +18,21 @@ public class EmployeeController {
 
 	@GetMapping("/")
 	public String viewHomePage(Model model) {
-			model.addAttribute("listEmployees", employeeService.getAllEmployees());
-			return "index";
-
-		}
+		model.addAttribute("listEmployees", employeeService.getAllEmployees());
+		return "index";
 
 	}
+	@GetMapping("/showNewEmployeeForm")
+	public String showNewEmployeeForm(Model model) {
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+		return "new_employee";
+	}
+
+	@PostMapping("/saveEmployee")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		// save employee to database
+		employeeService.saveEmployee(employee);
+		return "redirect:/";
+	}
+}
