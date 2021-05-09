73fcec6d91dfec25919ee532ce4497797684db20
Aryan Akshay <varyanakshay@gmail.com>
2021-05-09 20:35:53 +0530

    JPA Entity

--- src/main/java/com/example/springboot/model/Employee.java
+++ src/main/java/com/example/springboot/model/Employee.java
@@ -2,6 +2,7 @@ package com.example.springboot.model;
 
 import javax.persistence.*;
 
+@Entity
 @Table(name = "employees")
 public class Employee {
 
@@ -17,35 +18,27 @@ public class Employee {
 
     @Column(name = "email")
     private String email;
-
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
