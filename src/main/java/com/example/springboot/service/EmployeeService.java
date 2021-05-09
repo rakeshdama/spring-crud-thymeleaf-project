86dc7671992d594b68876a2b519b04973c68b261
rakeshdama <rakeshd0552@gmail.com>
2021-05-09 19:46:57 +0530

    Service, Model, Repository

---
+++ src/main/java/com/example/springboot/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.example.springboot.service;
+
+import com.example.springboot.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+}
